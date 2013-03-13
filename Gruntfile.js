40408247f6fd7e74e0f4d06acf44ccd94e9901d4
Billy Vong <billyvg@gmail.com>
2013-03-13 12:38:39 -0700

    Upgrade to grunt 0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,11 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     options: {
       testFiles: ['<%= pkg.name %>.v<%= pkg.version %>.min.js'],
       specFiles: ['test/spec/*.js']
     },
-    pkg: '<json:package.json>',
     meta: {
       banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
@@ -15,26 +15,11 @@ module.exports = function(grunt) {
         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
-    lint: {
-      files: ['grunt.js', '<%= pkg.name %>.v<%= pkg.version %>.js']
-    },
-    concat: {
-      dist: {
-        src: ['<%= pkg.name %>.v<%= pkg.version %>.js'],
-        dest: '<%= pkg.name %>.v<%= pkg.version %>.js'
-      }
-    },
-    min: {
-      dist: {
-        src: ['<config:concat.dist.dest>'],
-        dest: '<%= pkg.name %>.v<%= pkg.version %>.min.js'
-      }
-    },
     watch: {
       files: '<config:lint.files>',
       tasks: 'lint min',
       test: {
-        files: ['<config:options.testFiles>', '<config:options.specFiles>', 'test/TestRunner.html'],
+        files: ['<%= options.testFiles>', '<config:options.specFiles>', 'test/TestRunner.html'],
         tasks: 'mocha'
       }
     },
@@ -50,14 +35,31 @@ module.exports = function(grunt) {
         undef: true,
         boss: true,
         eqnull: true,
-        browser: true
+        browser: true,
+        globals: {
+          exports: true,
+          jQuery: true
+        }
       },
-      globals: {
-        exports: true,
-        jQuery: true
+      main: {
+          files: [{
+              src: ['grunt.js', '<%= pkg.name %>.v<%= pkg.version %>.js']
+          }]
       }
     },
-    uglify: {},
+    uglify: {
+        options: {
+            compressor: {
+                warnings: false
+            },
+            mangle: true,
+            compress: true
+        },
+        main: {
+            src: ['<%= pkg.name %>.js'],
+            dest: '<%= pkg.name %>.v<%= pkg.version %>.min.js'
+        }
+    },
     mocha: {
       all: {
         src: ['test/TestRunner.html'],
@@ -70,8 +72,10 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   // Default task.
-  grunt.registerTask('default', 'lint min mocha');
+  grunt.registerTask('default', 'jshint mocha uglify');
 
   grunt.task.registerTask('test', 'mocha');
 };
