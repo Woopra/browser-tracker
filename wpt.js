dcbe04eabc292cf136c5a74a6a08f97fcd778678
Billy Vong <billyvg@gmail.com>
2014-04-30 15:03:42 -0700

    Add polyfill for querySelectAll

--- wpt.js
+++ wpt.js
@@ -109,6 +109,30 @@
     })(window, document);
 
     /**
+     * Selectors API Level 1 (http://www.w3.org/TR/selectors-api/)
+     * http://ajaxian.com/archives/creating-a-queryselector-for-ie-that-runs-at-native-speed
+     */
+    if (!document.querySelectorAll) {
+        document.querySelectorAll = function (selectors) {
+            var style = document.createElement('style'), elements = [], element;
+            document.documentElement.firstChild.appendChild(style);
+            document._qsa = [];
+
+            style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
+            window.scrollBy(0, 0);
+            style.parentNode.removeChild(style);
+
+            while (document._qsa.length) {
+                element = document._qsa.shift();
+                element.style.removeAttribute('x-qsa');
+                elements.push(element);
+            }
+            document._qsa = null;
+            return elements;
+        };
+    }
+
+    /**
      * Helper functions
      */
     Woopra.extend = function(o1, o2) {
