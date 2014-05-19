0a0a6662362fb5f65f4aa3733c55098b2c5a3b72
Billy Vong <billyvg@gmail.com>
2014-05-19 14:16:05 -0700

    Remove duplicated indexOf polyfill

--- wpt.js
+++ wpt.js
@@ -19,41 +19,6 @@
     var XDM_PARAM_NAME = '__woopraid';
 
     /**
-     * Array.prototype.indexOf polyfill via
-     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
-     */
-    if (!Array.prototype.indexOf) {
-        Array.prototype.indexOf = function (searchElement, fromIndex) {
-            if ( this === undefined || this === null ) {
-                throw new TypeError( '"this" is null or not defined' );
-            }
-
-            var length = this.length >>> 0; // Hack to convert object.length to a UInt32
-
-            fromIndex = +fromIndex || 0;
-
-            if (Math.abs(fromIndex) === Infinity) {
-                fromIndex = 0;
-            }
-
-            if (fromIndex < 0) {
-                fromIndex += length;
-                if (fromIndex < 0) {
-                    fromIndex = 0;
-                }
-            }
-
-            for (;fromIndex < length; fromIndex++) {
-                if (this[fromIndex] === searchElement) {
-                    return fromIndex;
-                }
-            }
-
-            return -1;
-        };
-    }
-
-    /**
      * addEventListener polyfill 1.0 / Eirik Backer / MIT Licence
      * https://gist.github.com/eirikbacker/2864711
      */
