91d10bbd4105febcfe5f6a3eb2c68b26ba38a4af
Billy Vong <billyvg@gmail.com>
2014-06-16 16:58:18 -0700

    Edit gruntfile to upload multiple file versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,284 +3,335 @@ var spawn = require('child_process').spawn;
 
 module.exports = function(grunt) {
 
-  var TRACKER_FILENAME = 'w.js',
-      CDN_URL = 'http://static.woopra.com/';
+    var TRACKER_FILENAME = 'w';
 
+    var getVersion = function(version) {
+        var data = {};
+        var match, major, minor, patch;
 
-  var getVersion = function(version) {
-    var data = {};
-    var match, major, minor, patch;
+        match = version.match(/^([0-9]+)\.([0-9]+)\.([0-9]+).*/);
 
-    match = version.match(/^([0-9]+)\.([0-9]+)\.([0-9]+).*/);
-
-    if (match) {
-      data.major = match[1];
-      data.minor = match[2];
-      data.patch = match[3];
-    }
-
-    return data;
-  };
-
-  // Project configuration.
-  grunt.config.init({
-    pkg: grunt.file.readJSON('package.json'),
-    options: {
-      testFiles: ['<%= pkg.name %>.v<%= pkg.version %>.min.js'],
-      specFiles: ['test/spec/*.js']
-    },
-    connect: {
-        dev: {
-            options: {
-                port: 4242,
-                hostname: '0.0.0.0',
-                keepalive: true
-            }
-        },
-        test: {
-            options: {
-                port: 4141,
-                hostname: '0.0.0.0'
-            }
-        },
-    },
-
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint min',
-      test: {
-        files: ['<%= options.testFiles>', '<%= options.specFiles %>', 'test/TestRunner.html'],
-        tasks: 'mocha'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        unused: true,
-        globals: {
-          exports: true,
-          jQuery: true
+        if (match) {
+            data.major = match[1];
+            data.minor = match[2];
+            data.patch = match[3];
         }
-      },
-      main: {
-          files: [{
-              src: ['grunt.js', '<%= pkg.name %>.js']
-          }]
-      },
-      tests: {
-          options: {
-              globals: {
-                  exports: true,
-                  '$': true,
-                  describe: true,
-                  expect: true,
-                  before: true,
-                  beforeEach: true,
-                  after: true,
-                  afterEach: true,
-                  sinon: true,
-                  Woopra: true,
-                  WoopraTracker: true,
-                  it: true
-              }
-          },
-          files: [{
-              src: ['<%= options.specFiles %>']
-          }]
-      }
-    },
-    uglify: {
+
+        return data;
+    };
+
+    // Project configuration.
+    grunt.config.init({
+        CDN_URL: 'http://static.woopra.com',
+        filename: 'w',
+        pkg: grunt.file.readJSON('package.json'),
         options: {
-            compressor: {
-                warnings: false
+            testFiles: ['<%= pkg.name %>.v<%= pkg.version %>.min.js'],
+            specFiles: ['test/spec/*.js']
+        },
+        connect: {
+            dev: {
+                options: {
+                    port: 4242,
+                    hostname: '0.0.0.0',
+                    keepalive: true
+                }
             },
-            mangle: true,
-            compress: true
+            test: {
+                options: {
+                    port: 4141,
+                    hostname: '0.0.0.0'
+                }
+            },
         },
-        main: {
-          options: {
-          },
-            src: ['<%= pkg.name %>.js'],
-            dest: '<%= pkg.name %>.min.js'
+
+        meta: {
+            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+                '<%= grunt.template.today("yyyy-mm-dd") %>\\n' +
+                '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+                '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+                ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
         },
-        snippet: {
-            src: ['snippet.js'],
-            dest: 'snippet-min.js'
+        watch: {
+            files: '<config:lint.files>',
+            tasks: 'lint min',
+            test: {
+                files: ['<%= options.testFiles>', '<%= options.specFiles %>', 'test/TestRunner.html'],
+                tasks: 'mocha'
+            }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                unused: true,
+                globals: {
+                    exports: true,
+                    jQuery: true
+                }
+            },
+            main: {
+                files: [{
+                    src: ['grunt.js', '<%= pkg.name %>.js']
+                }]
+            },
+            tests: {
+                options: {
+                    globals: {
+                        exports: true,
+                        '$': true,
+                        describe: true,
+                        expect: true,
+                        before: true,
+                        beforeEach: true,
+                        after: true,
+                        afterEach: true,
+                        sinon: true,
+                        Woopra: true,
+                        WoopraTracker: true,
+                        it: true
+                    }
+                },
+                files: [{
+                    src: ['<%= options.specFiles %>']
+                }]
+            }
+        },
+        uglify: {
+            options: {
+                compressor: {
+                    warnings: false
+                },
+                mangle: true,
+                compress: true
+            },
+            main: {
+                options: {
+                },
+                src: ['<%= pkg.name %>.js'],
+                dest: '<%= pkg.name %>.min.js'
+            },
+            snippet: {
+                src: ['snippet.js'],
+                dest: 'snippet-min.js'
+            }
+        },
+        upload: {
+            tracker: {
+                type: 'scp',
+                host: 'woopra-cdn',
+                cdnRoot: '/var/www/html',
+                tasks: [{
+                    src: [
+                        '<%= uglify.main.dest %>'
+                    ],
+                    dest: '/js/<%= filename %>.js'
+                }]
+            },
+            dev: {
+                type: 'scp',
+                host: 'woopra-cdn',
+                cdnRoot: '/var/www/html',
+                tasks: [{
+                    src: [
+                        '<%= uglify.main.dest %>'
+                    ],
+                    dest: '/js/t/<%= filename %>-v<%= pkg.version %>.js'
+                }]
+
+            }
+        },
+        purge: {
+            tracker: {
+            },
+            dev: {
+            }
+        },
+        mocha: {
+            tracker: {
+                src: ['test/TestRunner.html'],
+                options: {
+                    mocha: {
+                        globals: ['woopra', 'newTracker', 'pingTracker', 'w1', 'w2', 'w3'],
+                        ignoreLeaks: false
+                    },
+                    reporter: 'Spec',
+                    run: true,
+                    log: true
+                }
+            },
+            snippet: {
+                src: ['test/snippet.html'],
+                options: {
+                    mocha: {
+                        globals: ['woopraLoaded', '_w', 'WoopraTracker', 'woopraTracker', 'WoopraLoadScript', 'Woopra', 'woopra', 'woopra_c1', 'woopra_c2', 'woopra_c3'],
+                        ignoreLeaks: false
+                    },
+                    reporter: 'Spec',
+                    run: true,
+                    log: false
+                }
+            }
         }
-    },
-    upload: {
-        tracker: {
-            type: 'scp',
-            host: 'woopra-cdn',
-            cdnRoot: '/var/www/html',
-            tasks: [{
+    });
+
+    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'test', 'uglify']);
+
+    grunt.task.registerTask('test', ['connect:test', 'mocha']);
+
+    grunt.registerTask('deploy', function() {
+        //grunt.task.run(['jshint', 'test']);
+        //grunt.task.run(['uglify']);
+
+
+        grunt.task.run('upload:dev');
+        grunt.task.run('purge:dev');
+    });
+
+    grunt.registerTask('version', function() {
+        var done = this.async();
+        var version = grunt.option('version') || 'patch';
+        var child;
+        var old = grunt.config('pkg.version');
+
+        child = spawn('npm', ['version', version]);
+
+        child.stdout.on('data', function(data) {
+            grunt.log.writeln(data);
+        });
+        child.stderr.on('data', function(data) {
+            grunt.log.error(data);
+        });
+
+        child.on('close', function(code) {
+            if (code === 0) {
+                grunt.config('pkg', grunt.file.readJSON('package.json'));
+                grunt.log.writeln('Updated version from ' + old + '->' + grunt.config('pkg.version') + ' (' + version + ')');
+            }
+            else {
+                grunt.log.error('Error updating version from ' + old + ' (' + version + ')');
+            }
+            done();
+        });
+    });
+
+    grunt.registerMultiTask('purge', 'Call EdgeCast API to purge CDN cache', function() {
+        var done = this.async();
+        var request = require('request');
+        var doRequest;
+        var upload;
+        var doneCounter = 0;
+        var doneInc;
+
+        doRequest = function(data) {
+            request.put({
+                url: 'https://api.edgecast.com/v2/mcc/customers/' + process.env.EDGECAST_ACCOUNT_NUMBER + '/edge/purge',
+                json: true,
+                body: data,
+                headers: {
+                    Authorization: 'TOK:' + process.env.EDGECAST_ACCESS_TOKEN,
+                    Accept: 'application/json'
+                }
+            }, function(e, r, body) {
+                var json;
+                if (!e) {
+                    if (body.Id) {
+                        // assume only one file gets updated for now
+                        grunt.log.writeln('Request sent, id = ' + body.Id);
+                        doneInc();
+                    }
+                    else {
+                        grunt.log.error('Error purging CDN cache.');
+                        done(false);
+                    }
+                }
+                else {
+                    grunt.log.error('Error purging CDN cache.');
+                    done(false);
+                }
+            });
+        };
+
+        upload = grunt.config('upload.' + this.target);
+
+        doneInc = function() {
+            doneCounter++;
+            if (doneCounter >= upload.tasks.length) {
+                done();
+            }
+        };
+
+        if (upload && upload.tasks) {
+            upload.tasks.forEach(function(task) {
+                grunt.log.writeln('Purging...', grunt.config('CDN_URL') + task.dest);
+                doRequest({
+                    MediaPath: grunt.config('CDN_URL') + task.dest,
+                    MediaType: 8
+                });
+            });
+        }
+    });
+
+    grunt.registerMultiTask('upload', 'Uploads to CDN', function() {
+        var self = this;
+        var done = this.async();
+        var versions;
+        var tasks;
+        var uploadTypes = {
+            scp: function() {
+                var scp = require('scp');
+
+                this.data.tasks.forEach(function(task) {
+                    task.src.forEach(function(src) {
+                        scp.send({
+                            host: self.data.host,
+                            path: self.data.cdnRoot + task.dest,
+                            file: __dirname + '/' + src
+                        }, function(err, stdout, stderr) {
+                            grunt.log.writeln(err, stdout, stderr);
+                            if (err) done(false);
+                            else done();
+                        });
+                        grunt.log.writeln('Uploading...', task.dest);
+                    });
+                });
+            }
+        };
+
+        if (this.target === 'dev') {
+            tasks = this.data.tasks;
+            versions = getVersion(grunt.config('pkg.version'));
+            tasks.push({
                 src: [
-                    '<%= uglify.main.dest %>'
+                    grunt.config('uglify.main.dest')
                 ],
-                dest: '<%= upload.tracker.cdnRoot %>/js/' + TRACKER_FILENAME
-            }]
-        },
-        dev: {
-            type: 'scp',
-            host: 'woopra-cdn',
-            cdnRoot: '/var/www/html',
-            tasks: [{
+                dest: '/js/t/' + grunt.config('filename') + '-v' + versions.major + '.js'
+            });
+            tasks.push({
                 src: [
-                    '<%= uglify.main.dest %>'
+                    grunt.config('uglify.main.dest')
                 ],
-                dest: '<%= upload.tracker.cdnRoot %>/js/t/' + TRACKER_FILENAME
-            }]
-
+                dest: '/js/t/' + grunt.config('filename') + '-v' + versions.major + '.' + versions.minor + '.js'
+            });
+            grunt.config(this.name + '.' + this.target + '.tasks', tasks);
         }
-    },
-    mocha: {
-      tracker: {
-        src: ['test/TestRunner.html'],
-        options: {
-          mocha: {
-            globals: ['woopra', 'newTracker', 'pingTracker', 'w1', 'w2', 'w3'],
-            ignoreLeaks: false
-          },
-          reporter: 'Spec',
-          run: true,
-          log: true
-        }
-      },
-      snippet: {
-        src: ['test/snippet.html'],
-        options: {
-          mocha: {
-            globals: ['woopraLoaded', '_w', 'WoopraTracker', 'woopraTracker', 'WoopraLoadScript', 'Woopra', 'woopra', 'woopra_c1', 'woopra_c2', 'woopra_c3'],
-            ignoreLeaks: false
-          },
-          reporter: 'Spec',
-          run: true,
-          log: false
-        }
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-mocha');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'test', 'uglify']);
-
-  grunt.task.registerTask('test', ['connect:test', 'mocha']);
-
-  grunt.registerTask('deploy', function() {
-      grunt.task.run(['jshint', 'test', 'uglify']);
-      grunt.task.run('upload:tracker');
-      grunt.task.run('purge');
-  });
-
-  grunt.registerTask('version', function() {
-    var done = this.async();
-    var version = grunt.option('version') || 'patch';
-    var child;
-    var old = grunt.config('pkg.version');
-
-    child = spawn('npm', ['version', version]);
-
-    child.stdout.on('data', function(data) {
-      grunt.log.writeln(data);
-    });
-    child.stderr.on('data', function(data) {
-      grunt.log.error(data);
+        uploadTypes[this.data.type].apply(this);
     });
 
-    child.on('close', function(code) {
-      if (code === 0) {
-        grunt.config('pkg', grunt.file.readJSON('package.json'));
-        grunt.log.writeln('Updated version from ', old, ' -> ', grunt.config('pkg.version'), ' (' + version + ')');
-      }
-      else {
-        grunt.log.error('Error updating version from ', old, ' (' + version + ')');
-      }
-      done();
-    });
-  });
-
-  grunt.registerTask('purge', 'Call EdgeCast API to purge CDN cache', function() {
-      var done = this.async(),
-          request = require('request'),
-          data = {
-              MediaPath: CDN_URL + 'js/' + TRACKER_FILENAME,
-              MediaType: 8
-          };
-
-      request.put({
-          url: 'https://api.edgecast.com/v2/mcc/customers/' + process.env.EDGECAST_ACCOUNT_NUMBER + '/edge/purge',
-          json: true,
-          body: data,
-          headers: {
-              Authorization: 'TOK:' + process.env.EDGECAST_ACCESS_TOKEN,
-              Accept: 'application/json'
-          }
-      }, function(e, r, body) {
-          var json;
-          if (!e) {
-              if (body.Id) {
-                  done();
-                  grunt.log.writeln('Request sent, id = ' + body.Id);
-              }
-              else {
-                  grunt.log.error('Error purging CDN cache.');
-              }
-          }
-          else {
-              grunt.log.error('Error purging CDN cache.');
-          }
-      });
-
-
-  });
-
-  grunt.registerMultiTask('upload', 'Uploads to CDN', function() {
-      var self = this;
-      var done = this.async();
-      var tasks = {
-          scp: function() {
-              var scp = require('scp');
-
-              this.data.tasks.forEach(function(task) {
-                  task.src.forEach(function(src) {
-                      scp.send({
-                          host: self.data.host,
-                          path: task.dest,
-                          file: __dirname + '/' + src
-                      }, function(err, stdout, stderr) {
-                          grunt.log.writeln(err, stdout, stderr);
-                          if (err) done(false);
-                          else done();
-                      });
-                  });
-              });
-          }
-      };
-
-      tasks[this.data.type].apply(this);
-  });
-  
 };
