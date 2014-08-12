e1b52999563cf63a70bd66e2d16ff2f0ed77c45a
Billy Vong <billyvg@gmail.com>
2014-08-12 14:25:10 -0700

    Fix serializing form data

--- wpt.js
+++ wpt.js
@@ -61,6 +61,69 @@
         }
     };
 
+    // https://code.google.com/p/form-serialize/
+    // modified to return an object
+    Woopra.serializeForm = function(form, exclude) {
+        if (!form || form.nodeName !== "FORM") {
+            return;
+        }
+        var i, j, data = {};
+        for (i = form.elements.length - 1; i >= 0; i = i - 1) {
+            if (form.elements[i].name === "" || exclude.indexOf(form.elements[i].name) > -1) {
+                continue;
+            }
+            switch (form.elements[i].nodeName) {
+                case 'INPUT':
+                    switch (form.elements[i].type) {
+                    case 'text':
+                        case 'hidden':
+                        case 'password':
+                        case 'button':
+                        case 'reset':
+                        case 'submit':
+                        data[form.elements[i].name] = form.elements[i].value;
+                    break;
+                    case 'checkbox':
+                        case 'radio':
+                        if (form.elements[i].checked) {
+                            data[form.elements[i].name] = form.elements[i].value;
+                        }
+                    break;
+                    case 'file':
+                        break;
+                }
+                break;
+                case 'TEXTAREA':
+                    data[form.elements[i].name] = form.elements[i].value;
+                break;
+                case 'SELECT':
+                    switch (form.elements[i].type) {
+                    case 'select-one':
+                        data[form.elements[i].name] = form.elements[i].value;
+                    break;
+                    case 'select-multiple':
+                        for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
+                        if (form.elements[i].options[j].selected) {
+                            data[form.elements[i].name] = form.elements[i].options[j].value;
+                        }
+                    }
+                    break;
+                }
+                break;
+                case 'BUTTON':
+                    switch (form.elements[i].type) {
+                    case 'reset':
+                        case 'submit':
+                        case 'button':
+                        data[form.elements[i].name] = form.elements[i].value;
+                    break;
+                }
+                break;
+            }
+        }
+        return data;
+    };
+
     /*!
      * jQuery Cookie Plugin v1.3.1
      * https://github.com/carhartl/jquery-cookie
@@ -846,40 +909,7 @@
                             event.returnValue = false;
                         }
 
-                        children = form.children;
-                        len = children ? children.length : 0;
-
-                        for (i = 0; i < len; i++) {
-                            if (!children.hasOwnProperty || children.hasOwnProperty(i)) {
-                                child = children[i];
-
-                                // track inputs with a non-empty name and value
-                                if (typeof child.name !== 'undefined' &&
-                                    typeof child.value !== 'undefined' &&
-                                    child.value !== '' &&
-                                    child.name !== '' &&
-                                    (!child.type || child.type !== 'password') &&
-                                    exclude.indexOf(child.name) < 0 &&
-                                    (child.type && child.type !== 'checkbox' || child.checked)) {
-
-                                    key = child.name;
-                                    value = child.value;
-                                    data = formData[key];
-
-                                    if (typeof data !== 'undefined') {
-                                        if (!data.push) {
-                                            data = [data];
-                                        }
-                                        data.push(value);
-                                    }
-                                    else {
-                                        data = value;
-                                    }
-
-                                    formData[key] = data;
-                                }
-                            }
-                        }
+                        formData = Woopra.serializeForm(form, exclude);
                         form.setAttribute('data-tracked', true);
                         self.track(_event, formData, function() {
                             form.submit();
